8063895f9ad228de06a297ef8c60a47573e44ef0
Thomas Kennedy <thomasmatthewkennedy@gmail.com>
2018-08-15 16:01:01 -0500

    started stocks display

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,30 @@
 <template>
-    <h1>/stocks/Stock component</h1>
+    <div class="col-sm-6 col-md-4">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                   <p>{{ stock.name }}</p> 
+                    <small>(Price: {{ stock.price }})</small>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="pull-left">
+                    <input 
+                        type="number" 
+                        class="form-control" 
+                        placeholder="quantity">
+                </div>
+                <div class="pull-right">
+                    <button class="btn btn-success">Buy</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    props: ['stock']
+}
+</script>
+
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,41 @@
 <template>
-    <h1>/stocks/Stocks component</h1>
+    <div>
+        <app-stock v-for='stock in stocks' :stock="stock"></app-stock>
+    </div>
 </template>
+
+<script>
+import Stock from './Stock.vue';
+export default {
+    data: function() {
+        return {
+            stocks: [
+                {
+                    id: 1,
+                    name: 'BMW',
+                    price : 40
+                },
+                {
+                    id: 2,
+                    name: 'Google',
+                    price : 95
+                },
+                {
+                    id: 3,
+                    name: 'Harley-Davidson',
+                    price : 35
+                },
+                {
+                    id: 1,
+                    name: 'Acme',
+                    price : 12
+                }
+            ]
+        }
+    },
+    components: {
+        'app-stock':Stock
+    }
+}
+</script>
+

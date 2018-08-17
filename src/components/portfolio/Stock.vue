e10712ce8407912700cf64c0b92ce3d92f5ed5e4
Thomas Kennedy <thomasmatthewkennedy@gmail.com>
2018-08-17 18:20:16 -0500

    added mapAction to sellstock

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="col-sm-6 col-md-4">
-        <div class="panel panel-success">
+        <div class="panel panel-info">
             <div class="panel-heading">
                 <h3 class="panel-title">
                    <p>{{ stock.name }}</p> 
-                    <small>(Price: {{ stock.price }} | Quantity: {{ quantity }})</small>
+                    <small>(Price: {{ stock.price }} | Quantity: {{ stock.quantity }})</small>
                 </h3>
             </div>
             <div class="panel-body">
@@ -17,7 +17,7 @@
                 </div>
                 <div class="pull-right">
                     <button 
-                        class="btn btn-success" 
+                        class="btn btn-info" 
                         @click="sellStock" 
                         :disabled="quantity < 1 || !Number.isInteger( parseFloat(quantity) )">Sell</button>
                 </div>
@@ -27,6 +27,7 @@
 </template>
 
 <script>
+import {mapActions} from 'vuex';
 export default {
     props: ['stock'],
     data() {
@@ -35,14 +36,23 @@ export default {
         }
     },
     methods: {
+        ...mapActions({
+            sellOrder: 'sellStock'
+        }),
         sellStock: function(){
             const order = {
                 stockId: this.stock.id,
                 stockPrice: this.stock.price,
                 stockQuantity: this.quantity
             };
-            //console.log(order);
-            this.$store.dispatch('sellStock', order);
+
+            // invoke the 'sellStock' action on portfolio.js, mapped to 'sellOrder' via mapActions above
+            this.sellOrder(order);
+
+            // could also just dispatch the action directly on the store
+            //this.$store.dispatch('sellStock', order);
+
+            this.quantity=0;
         }
     }
 }
